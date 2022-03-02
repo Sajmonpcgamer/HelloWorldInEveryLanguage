2d86ad1c3a98dded047f2355732ae4d1aae42ee2
Sajmonpcgamer <80468592+Sajmonpcgamer@users.noreply.github.com>
2022-03-02 11:18:54 +0100

    Added first files
    Currently:
    C++
    C
    C#
    Go
    Batch
    Brainfuck
    Binary
    Assembly
    swift
    R
    Python
    PHP
    Kotlin
    JavaScript
    Java
    Html

---
+++ hello.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace hellos
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
